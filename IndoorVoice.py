2433cf8f54772726fb67a4fd6dca44c32fa00f99
U20-hacktrail <noban.team@gmail.com>
2026-02-06 00:22:15 +0500

    lowercase all the text

---
+++ IndoorVoice.py
@@ -0,0 +1,3 @@
+ #Problem 1 WRITING IN ALL CAPS IS LIKE YELLING.Best to use your “indoor voice” sometimes, writing entirely in lowercase.
+introduction = input("Introduce yourself in 3 lines.").strip().lower()
+print(introduction)
